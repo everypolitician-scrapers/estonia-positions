fe47020f0c55bc513e4b180c4e5d92751e8b5078
Tony Bowden <tony@tmtm.com>
2016-10-01 18:46:56 +0100

    refactor: extract code into Modules

--- scraper.rb
+++ scraper.rb
@@ -4,43 +4,67 @@
 
 require 'everypolitician'
 require 'scraperwiki'
-require 'wikisnakker'
 
 require 'pry'
 
-house = EveryPolitician::Index.new.country('Estonia').legislature('Riigikogu')
-wanted = house.popolo.persons.map(&:wikidata).compact
+module Wikisnakker
+  class Item
+    QUALIFIERS = {
+      P102: :party,
+      P155: :follows,
+      P156: :followed_by,
+      P580: :start_date,
+      P582: :end_date,
+      P642: :of,
+      P768: :constituency,
+      P1365: :replaces,
+      P1366: :replaced_by,
+      P2937: :term,
+    }.freeze
 
-QUALIFIERS = {
-  P102: :party,
-  P155: :follows,
-  P156: :followed_by,
-  P580: :start_date,
-  P582: :end_date,
-  P642: :of,
-  P768: :constituency,
-  P1365: :replaces,
-  P1366: :replaced_by,
-  P2937: :term,
-}.freeze
+    def positions
+      return [] if self.P39s.empty?
+      self.P39s.map do |posn|
+        quals = posn.qualifiers
+        qdata = quals.properties.partition { |p| QUALIFIERS[p] }
+        qgood = qdata.first.map { |p| [ QUALIFIERS[p], quals[p].value.to_s ] }.to_h
+        warn "#{id}: #{posn.value.id} + unknown #{qdata.last.join(", ")}" unless qdata.last.empty?
 
-data = Wikisnakker::Item.find(wanted).map do |result|
-  next if (p39s = result.P39s).empty?
-  p39s.map do |posn|
-    quals = posn.qualifiers
-    qdata = quals.properties.partition { |p| QUALIFIERS[p] }
-    qgood = qdata.first.map { |p| [ QUALIFIERS[p], quals[p].value.to_s ] }.to_h
-    warn "#{result.id}: #{posn.value.id} + unknown #{qdata.last.join(", ")}" unless qdata.last.empty?
+        {
+          id:          id,
+          position:    posn.value.id,
+          label:       posn.value.to_s,
+          description: posn.value.description(:en).to_s,
+        }.merge(qgood) rescue {}
+      end
+    end
+  end
+end
 
-    {
-      id:          result.id,
-      position:    posn.value.id,
-      label:       posn.value.to_s,
-      description: posn.value.description(:en).to_s,
-    }.merge(qgood) rescue {}
+module Wikidata
+  require 'wikisnakker'
+
+  class Positions
+    def initialize(ids:)
+      @ids = ids
+    end
+
+    def positions
+      wikisnakker_items.flat_map(&:positions).compact
+    end
+
+    private
+
+    attr_reader :ids
+
+    def wikisnakker_items
+      @wsitems ||= Wikisnakker::Item.find(ids)
+    end
   end
 end
 
 
-
-
+house = EveryPolitician::Index.new.country('Estonia').legislature('Riigikogu')
+wanted = house.popolo.persons.map(&:wikidata).compact
+data = Wikidata::Positions.new(ids: wanted).positions
+warn data
